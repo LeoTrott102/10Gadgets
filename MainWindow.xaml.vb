5e60c68463a0ac54941278440ac4ad524dc52664
LeoTrott102 <43965291+LeoTrott102@users.noreply.github.com>
2021-01-07 21:58:25 +0000

    Not even alpha build
    This is VERY VERY Early and I'd love some ideas and contributions if you can :)

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class MainWindow
+    Private Const V As String = "C:\Program Files\10Gadgets\Images\calprev.png"
+    Private Const V1 As Integer = 10
+
+    Private Sub gs_cal_Click(sender As Object, e As RoutedEventArgs) Handles gs_cal.Click
+        calgag.Show()
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
---
+++ UsageMeter.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class UsageMeter
+
+End Class
